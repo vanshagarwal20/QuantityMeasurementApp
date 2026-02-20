4ba7c3b30c3d3525fc24e9ac950f0c41a9a33e58
vanshagarwal20 <vanshagarwal3329@gmail.com>
2026-02-20 15:51:16 +0530

    [Vansh] Add . uc4 extended unit support

---
+++ featureUC4-ExtendedUnitSupport/QuantityMeasurementApp/src/main/java/Measurement/QuantityMeasurementApp/FeetMeasurement.java
@@ -0,0 +1,105 @@
+package Measurement.QuantityMeasurementApp;
+
+public class FeetMeasurement {
+	
+	// UC1-Inner class to represent Feet measurement
+    public static class Feet {
+
+        private final double value;
+
+        // Constructor
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        public double getValue() {
+            return value;
+        }
+
+        
+        @Override
+        public boolean equals(Object obj) {
+
+            //Reference check
+            if (this == obj) {
+                return true;
+            }
+
+            //Null check
+            if (obj == null) {
+                return false;
+            }
+
+            //Type check
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+
+            //Cast safely
+            Feet other = (Feet) obj;
+
+            //Compare values using Double.compare()
+            return Double.compare(this.value, other.value) == 0;
+        }
+
+        // Recommended when overriding equals()
+        @Override
+        public int hashCode() {
+            return Double.hashCode(value);
+        }
+    }
+    // UC-2
+    public static class Inches {
+        private final double value;
+
+        public Inches(double value) {
+            if (Double.isNaN(value)) {
+                throw new IllegalArgumentException("Value must be numeric");
+            }
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+
+            if (this == obj)
+                return true;
+
+            if (obj == null)
+                return false;
+
+            if (getClass() != obj.getClass())
+                return false;
+
+            Inches other = (Inches) obj;
+
+            return Double.compare(this.value, other.value) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Double.hashCode(value);
+        }
+    }
+    
+    public static void demonstrateFeetEquality() {
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(1.0);
+
+        System.out.println("Input: 1.0 ft and 1.0 ft");
+        System.out.println("Output: Equal (" + f1.equals(f2) + ")");
+    }
+
+    public static void demonstrateInchesEquality() {
+        Inches i1 = new Inches(1.0);
+        Inches i2 = new Inches(1.0);
+
+        System.out.println("Input: 1.0 inch and 1.0 inch");
+        System.out.println("Output: Equal (" + i1.equals(i2) + ")");
+    }
+
+    public static void main(String[] args) {
+        demonstrateFeetEquality();
+        demonstrateInchesEquality();
+    }
+}
