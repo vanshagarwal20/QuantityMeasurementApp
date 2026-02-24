7361e28cb39ecfdc753dd5851222a4e4336306d6
vanshagarwal20 <vanshagarwal3329@gmail.com>
2026-02-24 12:37:40 +0530

    [vansh] Add . uc5 unit to unit conversion

---
+++ src/test/java/Measurement/QuantityMeasurementApp/InchTest.java
@@ -0,0 +1,44 @@
+package Measurement.QuantityMeasurementApp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import Measurement.QuantityMeasurementApp.FeetMeasurement.Feet;
+import Measurement.QuantityMeasurementApp.FeetMeasurement.Inches;
+
+public class InchTest {
+
+	 @Test
+	    public void testInchesEquality_SameValue() {
+	        Inches i1 = new Inches(5.0);
+	        Inches i2 = new Inches(5.0);
+	        assertTrue(i1.equals(i2));
+	    }
+
+	    @Test
+	    public void testInchesEquality_DifferentValue() {
+	        Inches i1 = new Inches(5.0);
+	        Inches i2 = new Inches(6.0);
+	        assertFalse(i1.equals(i2));
+	    }
+
+	    @Test
+	    public void testInchesEquality_NullComparison() {
+	        Inches i1 = new Inches(5.0);
+	        assertFalse(i1.equals(null));
+	    }
+
+	    @Test
+	    public void testInchesEquality_DifferentClass() {
+	        Inches i1 = new Inches(5.0);
+	        Feet f1 = new Feet(5.0);
+	        assertFalse(i1.equals(f1));
+	    }
+
+	    @Test
+	    public void testInchesEquality_SameReference() {
+	        Inches i1 = new Inches(5.0);
+	        assertTrue(i1.equals(i1));
+	    }
+}
