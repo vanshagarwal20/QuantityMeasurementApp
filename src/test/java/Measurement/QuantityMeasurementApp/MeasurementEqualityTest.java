7c0abe10e2b53341b6419dc22549848f563a9a22
vanshagarwal20 <vanshagarwal3329@gmail.com>
2026-03-09 16:14:44 +0530

    [vansh] Add . UC6 Addition Of Two Length Units

---
+++ src/test/java/Measurement/QuantityMeasurementApp/MeasurementEqualityTest.java
@@ -0,0 +1,74 @@
+package Measurement.QuantityMeasurementApp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class MeasurementEqualityTest {
+
+    // UC1 - Feet Tests
+    @Test
+    void testFeetEquality_SameValue() {
+        MeasurementEquality.Feet f1 = new MeasurementEquality.Feet(1.0);
+        MeasurementEquality.Feet f2 = new MeasurementEquality.Feet(1.0);
+        assertTrue(f1.equals(f2));
+    }
+
+    @Test
+    void testFeetEquality_DifferentValue() {
+        assertFalse(
+            new MeasurementEquality.Feet(1.0)
+                .equals(new MeasurementEquality.Feet(2.0))
+        );
+    }
+
+    @Test
+    void testFeetEquality_Null() {
+        assertFalse(new MeasurementEquality.Feet(1.0).equals(null));
+    }
+
+    @Test
+    void testFeetEquality_TypeMismatch() {
+        assertFalse(new MeasurementEquality.Feet(1.0).equals("abc"));
+    }
+
+    @Test
+    void testFeetEquality_SameReference() {
+        MeasurementEquality.Feet f = new MeasurementEquality.Feet(1.0);
+        assertTrue(f.equals(f));
+    }
+
+    // UC2 - Inch Tests
+    @Test
+    void testInchEquality_SameValue() {
+        assertTrue(
+            new MeasurementEquality.Inches(5.0)
+                .equals(new MeasurementEquality.Inches(5.0))
+        );
+    }
+
+    @Test
+    void testInchEquality_DifferentValue() {
+        assertFalse(
+            new MeasurementEquality.Inches(5.0)
+                .equals(new MeasurementEquality.Inches(6.0))
+        );
+    }
+
+    @Test
+    void testInchEquality_Null() {
+        assertFalse(new MeasurementEquality.Inches(5.0).equals(null));
+    }
+
+    @Test
+    void testInchEquality_TypeMismatch() {
+        assertFalse(new MeasurementEquality.Inches(5.0)
+                .equals(new MeasurementEquality.Feet(5.0)));
+    }
+
+    @Test
+    void testInchEquality_SameReference() {
+        MeasurementEquality.Inches i = new MeasurementEquality.Inches(5.0);
+        assertTrue(i.equals(i));
+    }
+}
