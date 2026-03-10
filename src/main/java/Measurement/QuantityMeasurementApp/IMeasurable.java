7d6b2524a07b54a563f02ff5e7a16fe1df2b7368
vanshagarwal20 <vanshagarwal3329@gmail.com>
2026-03-10 16:12:42 +0530

    [Vansh] Add . feature/UC13 Centralized Arithmetic Logic to Enforce DRY in Quantity Operations

---
+++ src/main/java/Measurement/QuantityMeasurementApp/IMeasurable.java
@@ -0,0 +1,16 @@
+package Measurement.QuantityMeasurementApp;
+
+public interface IMeasurable {
+
+    double getConversionFactor();
+
+    default double convertToBaseUnit(double value) {
+        return value * getConversionFactor();
+    }
+
+    default double convertFromBaseUnit(double baseValue) {
+        return baseValue / getConversionFactor();
+    }
+
+    String getUnitName();
+}
