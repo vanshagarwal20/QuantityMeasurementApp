b492ae3f2f1e20871703b7859b4ceb6a27532dc4
vanshagarwal20 <vanshagarwal3329@gmail.com>
2026-04-01 12:21:18 +0530

    [Vansh] Add . UC18 jwt and oauth

---
+++ src/main/java/com/app/quantitymeasurementapp/service/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.app.quantitymeasurementapp.service;
+
+import com.app.quantitymeasurementapp.model.User;
+import com.app.quantitymeasurementapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                Collections.emptyList()
+        );
+    }
+}
---
+++ src/main/java/com/app/quantitymeasurementapp/units/OperationType.java
@@ -0,0 +1,20 @@
+package com.app.quantitymeasurementapp.units;
+
+import java.util.function.DoubleBinaryOperator;
+
+public enum OperationType {
+
+    ADD((a, b) -> a + b),
+    SUBTRACT((a, b) -> a - b),
+    DIVIDE((a, b) -> a / b);
+
+    private final DoubleBinaryOperator operator;
+
+    OperationType(DoubleBinaryOperator operator) {
+        this.operator = operator;
+    }
+
+    public double apply(double a, double b) {
+        return operator.applyAsDouble(a, b);
+    }
+}
